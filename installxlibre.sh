76471ce27a037f0752bb0e6d4bb8a21998d71756
William <126926699+GitXpresso@users.noreply.github.com>
2025-09-15 11:38:16 +0000

    Create installxlibre.sh
    This is not tested yet...

---
+++ installxlibre.sh
@@ -0,0 +1,17 @@
+
+packages=( "x11proto-dev" "xtrans-dev" "libpixman-1-dev" "libxkbcommon-x11-dev" "libxfont-dev" "libxcvt-dev" "libdrm-dev" "libepoxy-dev" "x11proto-present-dev" "x11proto-dev" "libxkbfile-dev" "libudev-dev" "libxshmfence-dev" "libbsd-dev" "x11proto-xf86dri-dev" "libgl1-mesa-dev" "libglu1-mesa-dev" "libglu1-mesa-dev" "libgl-dev" "libdrm-dev" "xutils-dev" "mesa-common-dev" "libxcb-shape0-dev" "libxcb-util-dev" "libxcb-icccm4-dev")
+for package in "${packages[@]}" do;
+
+dpkg -L | grep ${packages[@]}
+
+  if [ "$?" -ne 0 ]; then
+    echo "${package} already installed"
+    sleep 0.2
+    clear
+  else
+    echo "${package} is not installed, installing ${package}"
+    sudo apt install -y ${package}
+    sleep 0.2
+    clear
+  fi
+done
