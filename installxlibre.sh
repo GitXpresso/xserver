f1dfa178aee8af01e5b395eaa65da7fdc91d7416
William <126926699+GitXpresso@users.noreply.github.com>
2025-09-15 12:48:10 +0000

    Update installxlibre.sh

--- installxlibre.sh
+++ installxlibre.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ -d /etc/apt ]; then
-packages=("x11proto-dev" "xtrans-dev" "libpixman-1-dev" "libxkbcommon-x11-dev" "libxfont-dev" "libxcvt-dev" "libdrm-dev" "libepoxy-dev" "x11proto-present-dev" "x11proto-dev" "libxkbfile-dev" "libudev-dev" "libxshmfence-dev" "libbsd-dev" "x11proto-xf86dri-dev" "libgl1-mesa-dev" "libglu1-mesa-dev" "libglu1-mesa-dev" "libgl-dev" "libdrm-dev" "xutils-dev" "mesa-common-dev" "libxcb-shape0-dev" "libxcb-util-dev" "libxcb-icccm4-dev")
+packages=("git" "x11proto-dev" "xtrans-dev" "libpixman-1-dev" "libxkbcommon-x11-dev" "libxfont-dev" "libxcvt-dev" "libdrm-dev" "libepoxy-dev" "x11proto-present-dev" "x11proto-dev" "libxkbfile-dev" "libudev-dev" "libxshmfence-dev" "libbsd-dev" "x11proto-xf86dri-dev" "libgl1-mesa-dev" "libglu1-mesa-dev" "libglu1-mesa-dev" "libgl-dev" "libdrm-dev" "xutils-dev" "mesa-common-dev" "libxcb-shape0-dev" "libxcb-util-dev" "libxcb-icccm4-dev")
  for package in "${packages[@]}"; do
 
  dpkg -l | grep -qw "${package}"
@@ -17,3 +17,13 @@ packages=("x11proto-dev" "xtrans-dev" "libpixman-1-dev" "libxkbcommon-x11-dev" "
   done
 fi
 echo "Done installing required packages..."
+sleep 0.2
+clear
+echo "checking if repository is already cloned..."
+if [ -f $(pwd)/xserver ]; then
+  echo "Repository already cloned, building XLibre from source..."
+else
+  echo "Repository not found, cloning repository..."
+  git clone https://github.com/gitxpresso/xserver $(pwd)
+fi
+  
